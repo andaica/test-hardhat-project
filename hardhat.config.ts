59788fb35688a8595f3010b8673633ac3436ba9f
an.nguyen <an.nguyen@dareplay.io>
2024-07-08 11:14:28 +0700

    first init

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import { HardhatUserConfig, task } from "hardhat/config";
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, { ethers }) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.formatEther(balance), "ETH");
+  });
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+export default config;
